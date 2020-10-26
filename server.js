21e0c0a824c17cd1ad7e503166af9215f1221bf3
usama v <varikkottilusama@gmail.com>
2020-10-26 22:17:39 +0530

    set ejs

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
-require("dotenv").config();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4} = require("uuid");
 
+// require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hi! Iam ready!");
+    res.redirect(`/${uuidV4()}`);
 });
 
-app.post("/", (req, res) => {
-    res.send("Hi! Post okay...!");
-});
+app.get("/:room", (req, res ) => {
+    res.render("room", {roomId: req.params.room});
+})
 
 
 const port = process.env.port || 5000
